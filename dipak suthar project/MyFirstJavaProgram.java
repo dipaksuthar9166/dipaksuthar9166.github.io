63e9e0b4f9afa1dadaafe6e233a525354797791e
Dipaksuthar9166.github.io <120955457+dipaksuthar9166@users.noreply.github.com>
2023-01-12 23:52:19 +0530

    Add files via upload
    Signed-off-by: Dipaksuthar9166.github.io <120955457+dipaksuthar9166@users.noreply.github.com>

---
+++ dipak suthar project/MyFirstJavaProgram.java
@@ -0,0 +1,17 @@
+public class MyFirstJavaProgram1
+{
+public static void main(String []args)
+{
+int a=25;
+int b=10;
+System.out.print("Addition of A + B is ");
+System.out.println(a+b);//15
+System.out.print("subtraction of A - B is ");
+System.out.println(a-b);//5
+System.out.print("Multiplication of A * B is ");
+System.out.println(a*b);//50
+System.out.print("division of A / B is ");
+System.out.println(a/b);//2
+System.out.print("A=B is ");
+System.out.println(a=b);
+}
